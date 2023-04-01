8b8f83627f55870c8dc6351dc4255348f0c16abc
mr.huangjian <mr.huangjian@foxmail.com>
2023-04-01 20:25:54 +0800

    m

--- node.js-electron/my-electron-app/eg-02/appMenu.js
+++ node.js-electron/my-electron-app/eg-02/appMenu.js
@@ -7,11 +7,17 @@ module.exports = function() {
             label: 'ElectronLabel',
             submenu: [
                 {
+                    
                     role: 'help',
                     accelerator: process.platform == 'darwin' ? 'Alt+Cmd+I' : 'Alt+Shift+I', // alt => option
                     click: () => {
                         shell.openExternal('https://www.electronjs.org/zh/')
                     },
+                },
+                {
+                    // TODO role的作用是添加系统菜单模板的菜单项，执行默认行为，比如复制粘贴、关于打开、最大最小关闭
+                    label: '关于',
+                    role: 'about'
                 },
                 {
                     type: 'separator',
